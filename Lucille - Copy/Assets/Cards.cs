8d3d568b7eb72889e8e023892c19452cf889bc49
Bot3east <sovisith1@gmail.com>
2018-10-17 10:32:36 -0500

    Lucille

---
+++ Lucille - Copy/Assets/Cards.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+    public Sprite artwork;
+
+    public int manaCost;
+    public int attack;
+    public int health;
+
+    public void Print()
+    {
+        Debug.Log("; " + " The card costs: " + manaCost);
+    }
+}
---
+++ Lucille - Copy/Assets/carddisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class carddisplay : MonoBehaviour {
+    public Card card;
+
+    public Text manaText;
+    public Text attackText;
+    public Text healthText;
+
+    
+	// Use this for initialization
+	void Start () {
+
+        manaText.text = card.manaCost.ToString();
+        attackText.text = card.attack.ToString();
+        healthText.text = card.health.ToString();
+	}
+
+
+}
