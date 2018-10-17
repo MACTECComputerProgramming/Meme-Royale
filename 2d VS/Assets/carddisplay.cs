0333b2d5198feb2192cdc8712557ce5e0b82bf0d
Bot3east <sovisith1@gmail.com>
2018-10-17 08:58:26 -0500

    Converting Cards 2 3d

---
+++ 2d VS/Assets/carddisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class carddisplay : MonoBehaviour {
+    public Card card;
+
+    public Text nameText;
+
+    public Image artworkImage;
+
+    public Text manaText;
+    public Text attackText;
+    public Text healthText;
+
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+}
