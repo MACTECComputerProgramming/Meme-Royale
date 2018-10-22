18fa272369e7e63b5ee69faf461ab6d3ab67d4bd
ColdAsMilk <32677787+ColdAsMilk@users.noreply.github.com>
2018-10-21 21:55:30 -0500

    Alot of stuff
    alot of deletes

---
+++ Lucille - Copy/Assets/Scripts/CardSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CardSpawner : NetworkBehaviour
+{
+
+    public GameObject cardPrefab;
+    public int numberOfCards;
+
+    public override void OnStartServer()
+    {
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            var spawnPosition = new Vector3(
+                Random.Range(-8.0f, 8.0f),
+                0.0f,
+                Random.Range(-8.0f, 8.0f));
+
+            var spawnRotation = Quaternion.Euler(
+                0.0f,
+                Random.Range(0, 180),
+                0.0f);
+
+            var enemy = (GameObject)Instantiate(cardPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(enemy);
+        }
+    }
+}
---
+++ Lucille - Copy/Assets/Scripts/CardsToPanels.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CardsToPanels : MonoBehaviour {
+
+
+    public GameObject cardPrefab;
+    public int numberOfCards;
+    public GameObject Player1Panel;
+
+    public void OnStartServer()
+    {
+        Player1Panel = GameObject.Find("Player1Hand");
+        GameObject cardPrefabInstance = Instantiate(cardPrefab) as GameObject; 
+        cardPrefabInstance.transform.parent = Player1Panel.transform;
+
+    }
+}
