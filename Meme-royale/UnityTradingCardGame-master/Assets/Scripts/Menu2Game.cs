3ab15f12ee7822871b79f83885d9bde4c8d26f4d
SomeDude44 <dillony19@gmail.com>
2018-11-07 10:53:15 -0600

    Revert "End Game"
    This reverts commit 27959d7d16276fd9a79c9dfe9966a383f3ee9966.

--- Meme-royale/UnityTradingCardGame-master/Assets/Scripts/Menu2Game.cs
+++ Meme-royale/UnityTradingCardGame-master/Assets/Scripts/Menu2Game.cs
@@ -15,9 +15,4 @@ public class Menu2Game : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 	
-    public void QuitGame()
-    {
-        Debug.Log("QUIT!");
-        Application.Quit();
-    }
 }
