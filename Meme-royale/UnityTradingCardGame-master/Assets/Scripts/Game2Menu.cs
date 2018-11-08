45fbb755b54c375058ac245db805a280cfcbdea7
SomeDude44 <dillony19@gmail.com>
2018-11-08 09:34:39 -0600

    End game buttons

--- Meme-royale/UnityTradingCardGame-master/Assets/Scripts/Game2Menu.cs
+++ Meme-royale/UnityTradingCardGame-master/Assets/Scripts/Game2Menu.cs
@@ -11,6 +11,11 @@ public class Game2Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
 
 
 }
